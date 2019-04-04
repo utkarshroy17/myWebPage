cc6f8da7e0c94b7771b19944c1823bf00f7fe5ba
Utkarsh Roy <utkarsh17r@gmail.com>
2019-04-03 23:02:03 -0400

    project tab complete

--- app.js
+++ app.js
@@ -20,21 +20,38 @@ $(document).ready(function(){
     $("#master-placeholder").load("https://raw.githubusercontent.com/utkarshroy17/myWebPage/master/master.html")
 });
 
+//document.getElementById("project-table").addEventListener("click", alterHeight);
 
+var tableHeight = 0, prevHeight;
+
+function alterHeight(acc, panel) {
+  //alert("changing height");    
+  if(!panel.style.maxHeight)
+    tableHeight += panel.scrollHeight;
+  else
+    tableHeight -= panel.scrollHeight;
+  document.getElementById("proj").style.height = tableHeight + 40 + 'px';
+}
 //project table
 document.addEventListener('DOMContentLoaded', function() {
     var acc = document.getElementsByClassName("accordion");
     var i;
+    
+    for (i = 0; i < acc.length; i++) {
+      tableHeight += acc[i].offsetHeight;
+    }
 
     for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        var panel = this.nextElementSibling;
-        if (panel.style.maxHeight){
-        panel.style.maxHeight = null;
-        } else {
-        panel.style.maxHeight = panel.scrollHeight + "px";
-        } 
-    });
+      acc[i].addEventListener("click", function() {
+          this.classList.toggle("active");
+          var panel = this.nextElementSibling;
+          alterHeight(acc, panel);
+          if (panel.style.maxHeight){
+            panel.style.maxHeight = null;
+          } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+          }          
+      });      
     }
+    
 }, false);
